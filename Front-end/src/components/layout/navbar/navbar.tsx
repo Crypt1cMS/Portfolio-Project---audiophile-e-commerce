ae0d9ac898d035e2cc9b67539c452f602bad2cf1
Crypt1cMS <dylan.minaya12@gmail.com>
2024-12-04 20:56:36 -0400

    Fixed Navbar and media query to the hero section

--- Front-end/src/components/layout/navbar/navbar.tsx
+++ Front-end/src/components/layout/navbar/navbar.tsx
@@ -2,6 +2,7 @@ import './navbar.css'
 import '../../../global.css'
 import Logo from '../../../../public/audiophile 2.svg'
 import Cart from '../../../../public/icon-cart.svg'
+import { Link } from 'react-router'
 
 function Navbar() {
 
@@ -9,20 +10,26 @@ function Navbar() {
       <>
         <nav>
 
-            <div className="main-container">
-                <img src={Logo} alt="Logo audiophile" />
+          <div className="nav-container">
 
-                <ul className='content'>
-                    <li>HOME</li>
-                    <li>HEADPHONES</li>
-                    <li>SPEAKERS</li>
-                    <li>EARPHONES</li>
-                </ul>
-
-                <img src={Cart} alt="Cart Icon" />
+            <div className="nav-logo">
+              <img src={Logo} alt="audiophile Logo" />
             </div>
 
-            <hr />
+            <ul>
+              <li><Link to={'/'}><p>HOMEPAGE</p></Link></li>
+              <li><Link to={'/headphones'}><p>HEADPHONES</p></Link></li>
+              <li><Link to={'/'}><p>EARPHONES</p></Link></li>
+              <li><Link to={'/'}><p>SPEAKERS</p></Link></li>
+            </ul>
+
+            <div className="nav-cart">
+              <img src={Cart} alt="Cart" />
+            </div>
+
+          </div>
+
+          <hr />
 
         </nav>
       </>
